1e437ab8b7bf73df43b03cf7e75444d2f69dd9e9
Dr.Harsh Maithani <harshmaithani09@gmail.com>
2021-09-01 17:08:03 +0200

    Add files via upload

--- vector.cpp
+++ vector.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <vector> 
 
+class MyClass
+{
+    public:
+        std::string name;
+        int         id;
+};
+
 int main(int, char**) {
     std::cout << "Vectors\n";
 
@@ -69,5 +76,96 @@ int main(int, char**) {
     
         std::cout << std::endl;
     }
+
+    // std::vector<int>
+
+    // Vector of vector of int
+
+    std::vector<std::vector<int>> intVectorVector;
+    std::vector<int> intVector;
+    int v = 100;
+    intVector.push_back(v);
+    intVectorVector.push_back(intVector);
+
+    // Vector of vector of pointers to int
+
+    std::vector<std::vector<int *>> intPointerVectorVector;
+    std::vector<int *> intPointerVector;
+    int* v2 = new int(100);
+    int* v3{new int(100)};
+
+    intPointerVector.push_back(v2);
+    intPointerVector.push_back(v3);
+
+    intPointerVectorVector.push_back(intPointerVector);
+    
+    for (auto i = intPointerVectorVector.begin(); i < intPointerVectorVector.end(); i++)
+    {
+        for (auto j = i->begin(); j < i->end(); j++)
+        {
+            std::cout << *(*j) << std::endl;
+        }
+    }
+
+    // Vector of vector of strings
+
+    std::vector<std::vector<std::string>> stringVectorVector;
+    std::vector<std::string> stringVector1;
+    std::vector<std::string> stringVector2;
+    std::string string1 = "harsh";
+    std::string string2 = "maithani";
+    stringVector1.push_back(string1);
+    stringVector1.push_back(string2);
+
+    std::string string3 = "alpana";
+    std::string string4 = "maithani";
+    stringVector2.push_back(string3);
+    stringVector2.push_back(string4);
+
+    stringVectorVector.push_back(stringVector1);
+    stringVectorVector.push_back(stringVector2);
+
+    for (auto i = stringVectorVector.begin(); i < stringVectorVector.end(); i++)
+    {
+        for (auto j = i->begin(); j < i->end(); j++)
+        {
+            std::cout << (*j) << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    // Vector of classes
+
+    std::vector<MyClass> classVector;
+    MyClass MyClassObject1;
+
+    classVector.push_back(MyClassObject1);
+    classVector[0].name             =       "harsh";
+    classVector[0].id               =       100;
+
+    for (auto i = classVector.begin(); i < classVector.end(); i++)
+    {
+        std::cout   <<      i->name     <<      std::endl;
+        std::cout   <<      (*i).id     <<      std::endl;
+    }
+
+    // Vector of pointers to classes
+
+    std::vector<MyClass *> classPointerVector;
+    classPointerVector.push_back(new MyClass);
+    classPointerVector[0]->name     =       "harsh";
+    classPointerVector[0]->id       =       100;
+
+    for (auto i = classPointerVector.begin(); i < classPointerVector.end(); i++)
+    {
+        std::cout   <<      (*i)->name      <<      std::endl;
+        std::cout   <<      (*i)->id        <<      std::endl; 
+    }
+
+    // Vector of vector of pointers
+
+    // Vector of vector of functors
+
+    // Vector of vector of templates
 
 }
