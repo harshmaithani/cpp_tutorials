5939ea499fa4abad6af7f5eb9b2c48d921ec9d7e
Dr.Harsh Maithani <harshmaithani09@gmail.com>
2021-09-02 17:43:34 +0200

    Add files via upload

--- pointers.cpp
+++ pointers.cpp
@@ -16,6 +16,8 @@ class Car
         {
             this->name = name;              // The this pointer is used to refere to self property inside a class
         }
+        int     price;
+        int*    wheels  = NULL;
         
 };
 
@@ -38,6 +40,9 @@ int callFunction(int x, int y, int (*function)(int,int))
 
 int main()
 {
+    // ++ Void pointer gets incremented by 1 byte
+    // ++ Type pointer gets incremented by the size of the type 
+
     int     a           =       100;        // variable of datatype int
     int*    b           =       &a;         // pointer to an int variable
     int*    c{&a};
@@ -76,14 +81,46 @@ int main()
         std::cout   <<  std::endl;
     }
 
-    Car     cp1;                            // Class
-    cp1.name = "Maruti";
+    Car     car1;                                   // Class on the stack
 
-    Car*    cp2;                            // Class
+    car1.name       =       "Maruti";
+    car1.price      =       1000;
+    int wheels      =       4;
+    car1.wheels     =       &wheels;
+    std::cout       <<  "Name of car is "           << car1.name        << std::endl;
+    std::cout       <<  "Price of the car is "      << car1.price       << std::endl; 
+    std::cout       <<  "Number of wheels is "      << *(car1.wheels)   << std::endl; 
+
+    Car     car2{"NoName",6000};
     
-    //cp2->name = "Maruti";                 // Simultaneously dereference the pointer and access the member function
-    //(*cp2).name = "Suzuki";               // First dereference, then access the function using dot notation
+    car2.name       =       "Honda";
+    car2.wheels     =       &wheels;
+    std::cout       <<  "Name of car is "           << car2.name        << std::endl;
+    std::cout       <<  "Price of the car is "      << car2.price       << std::endl;  
+    std::cout       <<  "Number of wheels is "      << *car2.wheels     << std::endl;
+    
+    Car*    car3 = new Car{"NoName",4000};          // Class on the heap. Allocate and initialize memory
 
+    car3->name      =       "Ford";                 // Simultaneously dereference the pointer and access the member function
+    (*car3).name    =       "Porsche";              // First dereference, then access the function using dot notation
+    car3->wheels    =       &wheels;
+    std::cout       <<  "Name of car is "           << car3->name       << std::endl;
+    std::cout       <<  "Price of the car is "      << car3->price      << std::endl;  
+    std::cout       <<  "Number of wheels is "      << *car3->wheels    << std::endl;
+
+    Car     car4    =       *car3;
+    Car*    car5    =       &car4;
+
+    void*   car6    =   static_cast<void*>(car3);   // Cast to a void pointer
+    Car*    car7    =   static_cast<Car*>(car6);    // Cast to a pointer to a class
+
+    std::cout       <<  "Name of car is "           << car7->name       << std::endl;
+    std::cout       <<  "Price of the car is "      << car7->price      << std::endl;  
+    std::cout       <<  "Number of wheels is "      << *car7->wheels    << std::endl;
+
+    delete car3;                                    // Don't forget!
+    //delete car5;                                  // Crash! Memory was already deleted!
+    
     int*    h{new int};                     // dynamically allocate on the heap and assign the address so we can access later
             *h          =       4;          // assign the value of 4 to allocated memory
     delete h;                               // return the memory pointed 
@@ -99,5 +136,52 @@ int main()
 
     std::cout << callFunction(5,3,Add)          << std::endl;   // Passing functions as arguments to other functions
     std::cout << callFunction(5,3,Subtract)     << std::endl;   // Passing functions as arguments to other functions
+    
+    int* ptr1;                              // Raw pointer. Invalid
+    int x = 100;
+    void* ptr2;                             // Void pointer. 
+    ptr2 = &x;
+
+    int* ptr3 = (int*)(ptr2);               // Casting from void pointer to int pointer 
+    int* ptr4 = NULL;                       // Null pointer
+    int* ptr5 = nullptr;                    // Null pointer
+    int* ptr6 = new int(100);               // 
+    delete ptr6;
+
+    int* ptr7 = new int[10];
+    delete[] ptr7;
+
+          int v  = 500;
+    const int v1 = 100;
+    const int v2 = 200;
+    const int* ptr8 = &v1;                  // Non-const pointer to a const int
+    // v1 = 500;                            // Won't work because v1 is a const
+    ptr8 = &v2;                             // Will work because ptr7 is a non-const pointer
+
+    const int* const ptr9 = &v1;            // Const pointer to a const int
+    //ptr9 = &v2;                           // Won't work because ptr8 is a const pointer
+
+    const int* volatile ptr10 = &v1;
+    std::cout   <<  "ptr10 points to "  <<      *ptr10      << std::endl;
+    ptr10 = &v2;
+    std::cout   <<  "ptr10 points to "  <<      *ptr10      << std::endl;  
+
+    int new_variable = 1000;
+
+    {
+        int* ptr11 = new int(new_variable);
+
+        std::cout       <<   "Ptr11 points to value " <<   *ptr11      <<      std::endl;
+
+        *ptr11 = 1500;
+
+        std::cout       <<   "New variable points to value " <<   new_variable      <<      std::endl;
+
+        std::unique_ptr<int> ptr12(ptr11);
+        
+        std::cout       <<   "Ptr12 points to value " <<   *ptr12      <<      std::endl;       // Smart pointer. Unique pointer does not share its pointer 
+
+        
+    } // Smart pointer deletes itself here
     
 }
